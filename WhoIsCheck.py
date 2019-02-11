e253d2f21e9e2d7499b3a5f204d0829f0cca8f07
Ted <tirthpatel86@gmail.com>
2019-02-11 00:30:42 -0800

    Update WhoIsCheck.py

--- WhoIsCheck.py
+++ WhoIsCheck.py
@@ -13,14 +13,14 @@ def whoisDsp():
     #Waiting for PC to boot up
     sleep(20)
     now = datetime.now()
-    domains = ['www.google.com', 'www.facebook.com']
+    domains = ['www.google.com', 'www.facebook.com', 'https://www.amazon.com/', 'ebay.com', 'www.stackoverflow.com', 'https://www.yahoo.com/']
 
     for doms in domains:
         # Getting WhoIs Information
         ins = whois.whois(doms)
-        dname = doms
-        dcreate = str(ins.creation_date)
+        dcreate = ins.creation_date
         dexplist = ins.expiration_date
+        dregistrar = ins.registrar
 
         if isinstance(dexplist,(list,)):
             # Runs the code below if domain has multiple expiration dates (Ex: Google, Yahoo or anything that uses MarkMonitor services)
@@ -48,14 +48,30 @@ def whoisDsp():
             dayremain = dayremain[:4]
 
         # Would you like a desktop notification or Terminal Output. [True for Notification | False for Output]
-        sendNotification = True
+        sendNotification = False
 
         if(sendNotification == True):
-            sendmessage(dname + " || " + dayremain + " Days")
+            sendmessage(doms + " || " + dayremain + " Days")
         else:
-            print("Domain Name: " + dname)
-            print("Domain  Reg: " + dcreate)
-            print("Domain  Exp: " + dexp)
-            print("Days    Rem: " + dayremain + "\n")
+            # Converts Expiration Date List to String if possible
+            if isinstance(dexplist,(list,)):
+                dexpDis = str(dexplist[0])
+            else:
+                dexpDis = str(dexplist)
+
+            # Converts Creation Date List to String if possible
+            if isinstance(dcreate,(list,)):
+                dcreateDis = str(dcreate[0])
+            else:
+                dcreateDis = str(dcreate)
+
+            print("Domain Name: \t" + doms)
+            print("Registrar: \t" + dregistrar)
+            print("Registration: \t" + dcreateDis)
+            print("Expiration: \t" + dexpDis)
+            print("Days Remaining: " + dayremain + "\n")
+
+        # Needs to 'Reset' or Sleep to do more than 3 searches quickly
+        sleep(1)
 
 whoisDsp()
